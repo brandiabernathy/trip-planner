0bd5eb256a934ffcdc94f586b43ff74911a57677
Brandi Abernathy <brandiabernathy22@gmail.com>
2024-04-26 21:09:44 -0700

    add new To Do list item

---
+++ src/components/ToDoForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import {
+	Button,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	FormControl,
+	FormLabel,
+	Input,
+	Stack,
+} from '@chakra-ui/react';
+import { db } from '../utils/firebase/config';
+import { doc, updateDoc } from "firebase/firestore";
+
+function ToDoForm({ isOpen, onClose, placeToEdit }) {
+    const [ newToDo, setNewToDo ] = useState({});
+
+	const submit = async () => {
+        const placeRef = doc(db, "places", placeToEdit.id);
+        try {
+            await updateDoc(placeRef, {
+                toDo: [
+                    ...placeToEdit.toDo,
+                    {...newToDo}
+                ]
+            });
+        }
+        catch(err) {
+            console.error(err);
+        }
+		onClose();
+	}
+
+	return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Things To Do</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Stack spacing={6}>
+                        <FormControl>
+                            <FormLabel>Description</FormLabel>
+                            <Input type="text" onChange={(e) => setNewToDo({...newToDo, description: e.target.value})}/>
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Link</FormLabel>
+                            <Input type="text" onChange={(e) => setNewToDo({...newToDo, link: e.target.value})}/>
+                        </FormControl>
+                    </Stack>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button mr={3} onClick={onClose}>Cancel</Button>
+                    <Button onClick={submit}>Save</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+	)
+}
+
+export default ToDoForm;
--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -1,14 +1,28 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Image, AspectRatio, Text, Heading, Link } from '@chakra-ui/react';
+import {
+    Box,
+    Image,
+    AspectRatio,
+    Text,
+    Heading,
+    Link,
+    useDisclosure,
+    Icon,
+    Flex,
+} from '@chakra-ui/react';
 import { db } from '../utils/firebase/config';
 import { getDocs, collection, query, where } from "firebase/firestore";
+import { FiPlusSquare, FiTrash2 } from "react-icons/fi";
 
 import Header from '../components/Header';
+import ToDoForm from '../components/ToDoForm';
 
 function Place() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const { shortCode } = useParams();
-    const [ place, setPlace ] = useState([]);
+    const [ place, setPlace ] = useState({});
+
 
     const placesCollectionRef = collection(db, "places");
 
@@ -17,7 +31,12 @@ function Place() {
             const q = query(placesCollectionRef, where("shortCode", "==", shortCode));
             const querySnapshot = await getDocs(q);
             querySnapshot.forEach((doc) => {
-                setPlace(doc.data());
+
+                setPlace({
+                    ...doc.data(),
+                    id: doc.id
+                });
+                console.log("doc", doc.data());
             });
 		}
 		catch(err) {
@@ -31,33 +50,58 @@ function Place() {
     }, [shortCode]);
 
 	return (
-		<Box className="place-page">
-			<Header />
-            <AspectRatio ratio={16 / 6} className="hero-img">
-                <Box>
-                    <Box className="overlay"></Box>
-                    <Image
-                        src={place.image}
-                        alt={place.name}
-                    />
-                    <Heading as="h2" className="container heading">{place.name}</Heading>
+        <>
+            <Box className="place-page">
+                <Header />
+                <AspectRatio ratio={16 / 6} className="hero-img">
+                    <Box>
+                        <Box className="overlay"></Box>
+                        <Image
+                            src={place.image}
+                            alt={place.name}
+                        />
+                        <Heading as="h2" className="container heading">{place.name}</Heading>
+                    </Box>
+                </AspectRatio>
+                <Box className="container">
+                    <Flex role="group" alignItems="center">
+                        <Heading as="h3">Things To Do</Heading>
+                        <Icon
+                            as={FiPlusSquare}
+                            onClick={onOpen}
+                            boxSize={8}
+                            display="none"
+                            cursor="pointer"
+                            ml={4}
+                            _groupHover={{
+                                display: "block"
+                            }}
+                        />
+                    </Flex>
+                    { place.hasOwnProperty('toDo') && place.toDo.map(item => {
+                        return (
+                            <Flex key={item.shortCode} role="group">
+                                { item.link ?
+                                    <Link href={item.link} target="_blank">{item.description}</Link>
+                                :
+                                    <Text>{item.description}</Text>
+                                }
+                                <Icon
+                                    as={FiTrash2}
+                                    display="none"
+                                    cursor="pointer"
+                                    ml={2}
+                                    _groupHover={{
+                                        display: "block"
+                                    }}
+                                />
+                            </Flex>
+                        )
+                    })}
                 </Box>
-            </AspectRatio>
-            <Box className="container">
-                <Heading as="h3">Things To Do</Heading>
-                { place.hasOwnProperty('toDo') && place.toDo.map(item => {
-                    return (
-                        <Box key={item.shortCode}>
-                            { item.link ?
-                                <Link href={item.link} target="_blank">{item.description}</Link>
-                            :
-                                <Text>{item.description}</Text>
-                            }
-                        </Box>
-                    )
-                })}
             </Box>
-		</Box>
+            { isOpen && <ToDoForm isOpen={isOpen} onClose={onClose} placeToEdit={place}/> }
+        </>
 	)
 }
 
